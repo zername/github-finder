2c5de4bfeca5498f73fa21e6fd2826eb419ca7f2
Paul <zerofield112@hotmail.com>
2020-07-17 13:34:25 +0200

    icon bug

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className='navbar bg-primary'>
+    <nav className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
       <ul>
         <li>
-          <Link to='/'>Home</Link>
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to="/about">About</Link>
         </li>
       </ul>
     </nav>
@@ -22,12 +22,12 @@ const Navbar = ({ icon, title }) => {
 
 Navbar.defaultProps = {
   title: 'Github Finder',
-  icon: 'fab fa-github'
+  icon: 'fa fa-github',
 };
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
 };
 
 export default Navbar;
